81169f79e5a8a3f6014022861727a3fc3eb6591a
DarkesJs <theyaemoribot@gmail.com>
2024-07-29 19:18:24 -0500

    Actualizar _welcome.js

--- plugins/_welcome.js
+++ plugins/_welcome.js
@@ -42,27 +42,4 @@ await conn.sendMini(m.chat, packname, dev, bye, img, img, redes, fkontak)
   if (chat.welcome && m.messageStubType == 32) {
     let kick = `┌─★ *Ai Yaemori - MD* \n│「 ADIOS 👋 」\n└┬★ 「 @${m.messageStubParameters[0].split`@`[0]} 」\n   │✑  Se fue\n   │✑ Jamás te quisimos aquí\n   └───────────────┈ ⳹`
 await conn.sendMini(m.chat, packname, dev, kick, img, img, redes, fkontak)
-}}
-
-/*import {WAMessageStubType} from '@whiskeysockets/baileys'
-import fetch from 'node-fetch'
-
-export async function before(m, {conn, participants, groupMetadata}) {
-  if (!m.messageStubType || !m.isGroup) return !0;
-  let img = imagen1
-  let chat = global.db.data.chats[m.chat]
-
-  if (chat.welcome && m.messageStubType == 27) {
-    let welcome = `Bienvenido @${m.messageStubParameters[0].split`@`[0]}`
-await conn.sendMini(m.chat, packname, dev, welcome, img, img, redes, fkontak)
-  }
-
-  if (chat.welcome && m.messageStubType == 28) {
-    let bye = `Adiós @${m.messageStubParameters[0].split`@`[0]}`
-await conn.sendMini(m.chat, packname, dev, bye, img, img, redes, fkontak)
-  }
-
-  if (chat.welcome && m.messageStubType == 32) {
-    let kick = `Adiós @${m.messageStubParameters[0].split`@`[0]}`
-await conn.sendMini(m.chat, packname, dev, kick, img, img, redes, fkontak)
-}}*/*/
+}}*/
