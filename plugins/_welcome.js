7430161173d53dd0a1b931359e006563312d0374
Ofc.Diego <151096440+OfcDiego@users.noreply.github.com>
2024-07-30 07:57:45 -0500

    Actualizar _welcome.js

--- plugins/_welcome.js
+++ plugins/_welcome.js
@@ -2,6 +2,26 @@ import {WAMessageStubType} from '@whiskeysockets/baileys'
 import fetch from 'node-fetch'
 
 export async function before(m, {conn, participants, groupMetadata}) {
+if (!m.messageStubType || !m.isGroup) return !0
+let vn = 'https://qu.ax/cUYg.mp3'
+let img = imagen1
+let chat = global.db.data.chats[m.chat]
+
+if (chat.welcome && m.messageStubType == 27) {
+this.sendMessage(m.chat, { audio: { url: vn }, contextInfo:{ mentionedJid:[nombre], "externalAdReply": { "thumbnail": img, "title": `乂 Ｗ Ｅ Ｌ Ｃ Ｏ Ｍ Ｅ 乂`, "body": dev, "previewType": "PHOTO", "thumbnailUrl": null, "showAdAttribution": true,  sourceUrl: [tiktok, yt, md, channel].getRandom()}},  ptt: true, mimetype: 'audio/mpeg', fileName: `error.mp3` }, { quoted: fkontak })}
+
+if (chat.welcome && m.messageStubType == 28) {
+this.sendMessage(m.chat, { text: `Se fue @${m.messageStubParameters[0].split`@`[0]} nadie lo va ha extrañar 😹`,
+contextInfo:{ forwardedNewsletterMessageInfo: { newsletterJid: '120363263466636910@newsletter', serverMessageId: '', newsletterName: namechannel }, forwardingScore: 9999999, isForwarded: true, mentionedJid:[nombre], "externalAdReply": {"showAdAttribution": true, "containsAutoReply": true, "title": '乂 Ａ Ｄ Ｉ Ｏ́ Ｓ 乂', body: `Esperemos que no vuelva -_-`, "previewType": "PHOTO", "thumbnailUrl": ``, "thumbnail": img, "sourceUrl": `${redes}`}}}, {quoted: null, ephemeralExpiration: 24*60*100, disappearingMessagesInChat: 24*60*100})}
+
+if (chat.welcome && m.messageStubType == 32) {
+this.sendMessage(m.chat, { text: `Se fue @${m.messageStubParameters[0].split`@`[0]} nadie lo va ha extrañar 😹`,
+contextInfo:{ forwardedNewsletterMessageInfo: { newsletterJid: '120363263466636910@newsletter', serverMessageId: '', newsletterName: namechannel }, forwardingScore: 9999999, isForwarded: true, mentionedJid:[nombre], "externalAdReply": {"showAdAttribution": true, "containsAutoReply": true, "title": '乂 Ａ Ｄ Ｉ Ｏ́ Ｓ 乂', body: `Esperemos que no vuelva -_-`, "previewType": "PHOTO", "thumbnailUrl": ``, "thumbnail": img, "sourceUrl": `${redes}`}}}, {quoted: null, ephemeralExpiration: 24*60*100, disappearingMessagesInChat: 24*60*100})}}
+
+/*import {WAMessageStubType} from '@whiskeysockets/baileys'
+import fetch from 'node-fetch'
+
+export async function before(m, {conn, participants, groupMetadata}) {
   if (!m.messageStubType || !m.isGroup) return !0;
   let img = imagen1
   let chat = global.db.data.chats[m.chat]
@@ -19,24 +39,4 @@ await conn.sendMini(m.chat, packname, dev, bye, img, img, redes, fkontak)
   if (chat.welcome && m.messageStubType == 32) {
     let kick = `┌─★ *Ai Yaemori - MD* \n│「 ADIOS 👋 」\n└┬★ 「 @${m.messageStubParameters[0].split`@`[0]} 」\n   │✑  Se fue\n   │✑ Jamás te quisimos aquí\n   └───────────────┈ ⳹`
 await conn.sendMini(m.chat, packname, dev, kick, img, img, redes, fkontak)
-}}
-
-/*import {WAMessageStubType} from '@whiskeysockets/baileys'
-import fetch from 'node-fetch'
-
-export async function before(m, {conn, participants, groupMetadata}) {
-if (!m.messageStubType || !m.isGroup) return !0
-let vn = 'https://qu.ax/cUYg.mp3'
-let img = imagen1
-let chat = global.db.data.chats[m.chat]
-
-if (chat.welcome && m.messageStubType == 27) {
-this.sendMessage(m.chat, { audio: { url: vn }, contextInfo:{ mentionedJid:[nombre], "externalAdReply": { "thumbnail": img, "title": `乂 Ｗ Ｅ Ｌ Ｃ Ｏ Ｍ Ｅ 乂`, "body": dev, "previewType": "PHOTO", "thumbnailUrl": null, "showAdAttribution": true,  sourceUrl: [tiktok, yt, md, channel].getRandom()}},  ptt: true, mimetype: 'audio/mpeg', fileName: `error.mp3` }, { quoted: fkontak })}
-
-if (chat.welcome && m.messageStubType == 28) {
-this.sendMessage(m.chat, { text: `Se fue @${m.messageStubParameters[0].split`@`[0]} nadie lo va ha extrañar 😹`,
-contextInfo:{ forwardedNewsletterMessageInfo: { newsletterJid: '120363263466636910@newsletter', serverMessageId: '', newsletterName: namechannel }, forwardingScore: 9999999, isForwarded: true, mentionedJid:[nombre], "externalAdReply": {"showAdAttribution": true, "containsAutoReply": true, "title": '乂 Ａ Ｄ Ｉ Ｏ́ Ｓ 乂', body: `Esperemos que no vuelva -_-`, "previewType": "PHOTO", "thumbnailUrl": ``, "thumbnail": img, "sourceUrl": `${redes}`}}}, {quoted: null, ephemeralExpiration: 24*60*100, disappearingMessagesInChat: 24*60*100})}
-
-if (chat.welcome && m.messageStubType == 32) {
-this.sendMessage(m.chat, { text: `Se fue @${m.messageStubParameters[0].split`@`[0]} nadie lo va ha extrañar 😹`,
-contextInfo:{ forwardedNewsletterMessageInfo: { newsletterJid: '120363263466636910@newsletter', serverMessageId: '', newsletterName: namechannel }, forwardingScore: 9999999, isForwarded: true, mentionedJid:[nombre], "externalAdReply": {"showAdAttribution": true, "containsAutoReply": true, "title": '乂 Ａ Ｄ Ｉ Ｏ́ Ｓ 乂', body: `Esperemos que no vuelva -_-`, "previewType": "PHOTO", "thumbnailUrl": ``, "thumbnail": img, "sourceUrl": `${redes}`}}}, {quoted: null, ephemeralExpiration: 24*60*100, disappearingMessagesInChat: 24*60*100})}}*/
+}}*/
