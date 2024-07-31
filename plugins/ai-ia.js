4272446eeaf6121c89569a646755218c6b983430
Ofc.Diego <151096440+OfcDiego@users.noreply.github.com>
2024-07-30 21:25:32 -0500

    Actualizar ai-ia.js

--- plugins/ai-ia.js
+++ plugins/ai-ia.js
@@ -1,7 +1,7 @@
 import axios from 'axios';
 import fetch from 'node-fetch';
 
-let handler = async (m, { conn, text }) => {
+let handler = async (m, { conn, usedPrefix, command text }) => {
     const isQuotedImage = m.quoted && (m.quoted.msg || m.quoted).mimetype && (m.quoted.msg || m.quoted).mimetype.startsWith('image/');
     const username = `${conn.getName(m.sender)}`;
     const basePrompt = `Tu nombre es Ai Yaemori y parece haber sido creado por OfcDiego. Tú usas el idioma Español. Llamarás a las personas por su nombre ${username}, te gusta ser divertida, y te encanta aprender. Lo más importante es que debes ser amigable con la persona con la que estás hablando. ${username}`;
