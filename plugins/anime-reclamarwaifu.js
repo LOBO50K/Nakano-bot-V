085ac3476718909aeadf05e76313a56f6577e332
Ofc.Diego <151096440+OfcDiego@users.noreply.github.com>
2024-07-31 17:50:21 -0500

    Actualizar anime-reclamarwaifu.js

--- plugins/anime-reclamarwaifu.js
+++ plugins/anime-reclamarwaifu.js
@@ -18,9 +18,9 @@ const roll = Math.floor(Math.random() * 100) + 1
 
 // Si el roll es mayor o igual a 50, ganamos la waifu
 if (roll >= 10) {
-m.reply("😊 ¡Felicidades! Has ganado la waifu 🌟")
+conn.reply(m.chat, '😊 ¡Felicidades! Has ganado la waifu 🌟', m, fake)
 } else {
-m.reply("⭐️ Lo siento, no has ganado la waifu. ¡Inténtalo de nuevo!")}}
+conn.reply(m.chat, '⭐️ Lo siento, no has ganado la waifu. ¡Inténtalo de nuevo!', m, fake)}}
 
 // Llamamos a la función para reclamar la waifu
 reclamarWaifu()}
