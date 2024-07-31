f05728ca3fa138739741b9c9c8cfaa1da6502253
Ofc.Diego <151096440+OfcDiego@users.noreply.github.com>
2024-07-31 17:57:02 -0500

    Actualizar anime-reclamarwaifu.js

--- plugins/anime-reclamarwaifu.js
+++ plugins/anime-reclamarwaifu.js
@@ -7,17 +7,20 @@
 
 //aun no funciona correctamente.
 
-// Configuraci��n del reclamo
-const waifuName = "Ai Yaemori";
-const reclamoMensaje = `¡Hola! Reclamo a ${waifuName} como mi waifu.`;
+var handler = async (m, { text,  usedPrefix, command }) => {
 
 // Función para reclamar la waifu
 function reclamarWaifu() {
-  m.reply(reclamoMensaje);
-  alert(reclamoMensaje);
-}
+// Simulamos un roll de dados para determinar si ganamos la waifu
+const roll = Math.floor(Math.random() * 100) + 1
 
-// Llamar a la función para reclamar la waifu
+// Si el roll es mayor o igual a 50, ganamos la waifu
+if (roll >= 10) {
+m.reply("😊 ¡Felicidades! Has ganado la waifu 🌟")
+} else {
+m.reply("⭐️ Lo siento, no has ganado la waifu. ¡Inténtalo de nuevo!")}}
+
+// Llamamos a la función para reclamar la waifu
 reclamarWaifu()}
 
 handler.command = ['c', 'reclamar']
