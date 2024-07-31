289de808f0d60bc916a672ed6c97d9fbbdf4a033
Ofc.Diego <151096440+OfcDiego@users.noreply.github.com>
2024-07-31 09:10:30 -0500

    Actualizar anime-rollwaifu.js

--- plugins/anime-rollwaifu.js
+++ plugins/anime-rollwaifu.js
@@ -9,7 +9,8 @@ const waifus = [
   // Agrega más waifus a la lista!
 ];
 
-conn.reply('message', async message => {
+handler.command = ['c']
+//conn.reply('message', async message => {
   if (message.type === 'chat' && message.body.startsWith('!c ')) {
     const waifuNombre = message.body.substring(3).trim();
     const waifu = waifus.find(waifu => waifu.nombre.toLowerCase() === waifuNombre.toLowerCase());
