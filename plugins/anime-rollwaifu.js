196f1e715fb432d35b504f20c60c1a76a73fc27f
Ofc.Diego <151096440+OfcDiego@users.noreply.github.com>
2024-07-31 09:36:27 -0500

    Actualizar anime-rollwaifu.js

--- plugins/anime-rollwaifu.js
+++ plugins/anime-rollwaifu.js
@@ -17,7 +17,7 @@ return waifus[indiceAleatorio]}
 
 // Función para mostrar la waifu obtenida
 function mostrarWaifu(waifu) {
-m.reply(`Tu waifu es: ${waifu.nombre} de ${waifu.anime}`)}
+conn.reply(m.chat. `Tu waifu es: ${waifu.nombre} de ${waifu.anime}`, m, rcanal)}
 
 // Ejecuta la función para obtener y mostrar una waifu aleatoria
 const waifuAleatoria = obtenerWaifuAleatoria();
