ce3ea1c37835621c027b45f30a1b6aa1d71406d5
Ofc.Diego <151096440+OfcDiego@users.noreply.github.com>
2024-07-31 09:35:35 -0500

    Actualizar anime-rollwaifu.js

--- plugins/anime-rollwaifu.js
+++ plugins/anime-rollwaifu.js
@@ -1,34 +1,30 @@
-// RollWaifu.js
+var handler = async (m, { text,  usedPrefix, command }) => {
 
-// Array de waifus
+//rollwaifus
 const waifus = [
-  { nombre: 'Mikasa Ackerman', anime: 'Shingeki no Kyojin' },
-  { nombre: 'Asuna Yuuki', anime: 'Sword Art Online' },
-  { nombre: 'Hestia', anime: 'DanMachi' },
-  { nombre: 'Rias Gremory', anime: 'High School DxD' },
-  { nombre: 'Saber', anime: 'Fate/stay night' },
-  // Agrega más waifus aquí...
-];
+{ nombre: 'Mikasa Ackerman', anime: 'Shingeki no Kyojin' },
+{ nombre: 'Asuna Yuuki', anime: 'Sword Art Online' },
+{ nombre: 'Hestia', anime: 'DanMachi' },
+{ nombre: 'Rias Gremory', anime: 'High School DxD' },
+{ nombre: 'Saber', anime: 'Fate/stay night' },
+// Agrega más waifus aquí...
+]
 
 // Función para obtener una waifu aleatoria
 function obtenerWaifuAleatoria() {
-  const indiceAleatorio = Math.floor(Math.random() * waifus.length);
-  return waifus[indiceAleatorio];
-}
+const indiceAleatorio = Math.floor(Math.random() * waifus.length)
+return waifus[indiceAleatorio]}
 
 // Función para mostrar la waifu obtenida
 function mostrarWaifu(waifu) {
-  console.log(`Tu waifu es: ${waifu.nombre} de ${waifu.anime}`);
-}
+m.reply(`Tu waifu es: ${waifu.nombre} de ${waifu.anime}`)}
 
 // Ejecuta la función para obtener y mostrar una waifu aleatoria
 const waifuAleatoria = obtenerWaifuAleatoria();
-mostrarWaifu(waifuAleatoria);
+mostrarWaifu(waifuAleatoria)}
 
-/*};
-handler.help = ['rollwaifu'];
-handler.tags = ['anime'];
-handler.command = ['rw', 'rollwaifu'];
-handler.register = true;
-
-export default handler;*/
+handler.command = ['rw', 'rollwaifu']
+handler.help = ['rw']
+handler.tags = ['anime']
+handler.premium = false
+export default handler
