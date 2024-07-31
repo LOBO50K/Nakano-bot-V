f036c96b46057d793ceb5fd4aefdd86fceb98518
Ofc.Diego <151096440+OfcDiego@users.noreply.github.com>
2024-07-31 10:05:31 -0500

    Actualizar anime-reclamarwaifu.js

--- plugins/anime-reclamarwaifu.js
+++ plugins/anime-reclamarwaifu.js
@@ -1,4 +1,4 @@
-/*var handler = async (m, { text,  usedPrefix, command }) => {
+var handler = async (m, { text,  usedPrefix, command }) => {
 
 // Clase Waifu
 class Waifu {
@@ -32,48 +32,6 @@ const dinero = 250;
 const waifuComprada = tienda.comprarWaifu("Mikasa", dinero)}
 
 handler.command = ['c', 'reclamar']
-handler.help = ['c']
-handler.tags = ['anime']
-handler.premium = false
-export default handler*/
-
-var handler = async (m, { text,  usedPrefix, command }) => {
-
-
-// Clase Rollwaifu para representar a la rollwaifu
-class Rollwaifu {
-  constructor(nombre, edad, personalidad) {
-    this.nombre = nombre;
-    this.edad = edad;
-    this.personalidad = personalidad;
-    this.reclamada = false;
-    this.dueno = null;
-  }
-
-  reclamar(dueno) {
-    if (!this.reclamada) {
-      this.reclamada = true;
-      this.dueno = dueno;
-      return `¡Felicitaciones, ${dueno}! Has reclamado a ${global.anime}.`;
-    } else {
-      return `Lo siento, ${this.nombre} ya ha sido reclamada por ${this.dueno}.`;
-    }
-  }
-}
-
-// Creamos una instancia de la clase Rollwaifu
-const rollwaifu = new Rollwaifu("Ai", 20, "amable y divertida");
-
-// Función para reclamar la rollwaifu
-function reclamarRollwaifu(dueno) {
-  const respuesta = rollwaifu.reclamar(dueno);
-  m.reply(respuesta);
-}
-
-// Llamamos a la función para reclamar la rollwaifu
-reclamarRollwaifu("YL")}
-
-handler.command = ['c', 'reclamar', 'comprar']
 handler.help = ['c']
 handler.tags = ['anime']
 handler.premium = false
