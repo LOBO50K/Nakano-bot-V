188c8adf0634fbcf33754c54f276a806d126887c
Ofc.Diego <151096440+OfcDiego@users.noreply.github.com>
2024-08-02 21:01:29 -0500

    Actualizar jadibot-deletebot.js

--- plugins/jadibot-deletebot.js
+++ plugins/jadibot-deletebot.js
@@ -8,10 +8,10 @@ let uniqid = `${who.split`@`[0]}`
 let userS = `${conn.getName(who)}`
 
 try {
-await fs.rmdir("./LuffyJadiBot/" + uniqid, { recursive: true, force: true })
+await fs.rmdir("./MiniJadiBot/" + uniqid, { recursive: true, force: true })
 await parentw.sendMessage(m.chat, { text: '🚩 Sub-Bot eliminado.' }, { quoted: fkontak })
 } catch(err) {
-if (err.code === 'ENOENT' && err.path === `./LuffyJadiBot/${uniqid}`) {
+if (err.code === 'ENOENT' && err.path === `./MiniJadiBot/${uniqid}`) {
 await parentw.sendMessage(m.chat, { text: "🍟 No cuentas con ninguna sesión de Sub-Bot." }, { quoted: fkontak })
 } else {
 await m.react(error)
