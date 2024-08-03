064287648f80b4ec21c1e5422cd7f13d87b0531c
Ofc.Diego <151096440+OfcDiego@users.noreply.github.com>
2024-08-02 19:23:28 -0500

    Actualizar nsfw-r34.js

--- plugins/nsfw-r34.js
+++ plugins/nsfw-r34.js
@@ -1,11 +1,12 @@
 import { igdl } from 'ruhend-scraper'
 let handler = async (m, { conn, text, usedPrefix, command }) => {
-if (!global.db.data.chats[m.chat].modohorny) return conn.reply(m.chat, `🚩 El grupo no admite contenido *Nsfw.*`, m, rcanal)
+if (!db.data.chats[m.chat].modohorny && m.isGroup) return conn.reply(m.chat, `🚩 El grupo no admite contenido *Nsfw.*`, m, rcanal)
 if (!text) return m.reply('🚩 Ingresa el nombre de la imágen que estas buscando.')
 await m.react(rwait)
 try {
-let { dl_url } = await Starlights.rule34(text)
-await conn.sendFile(m.chat, dl_url, 'thumbnail.jpg', `*» Resultado* : ${text}`, m, null, rcanal)
+//let { dl_url } = await Starlights.rule34(text)
+const url = `https://rule34.xxx/index.php?page=dapi&s=post&q=index&json=1&tags=${use}`;
+await conn.sendFile(m.chat, url, 'thumbnail.jpg', `*» Resultado* : ${text}`, m, null, rcanal)
 await m.react(done)
 } catch {
 conn.reply(m.chat, '🚩 Ocurrió un error', m, fake)
