e577e4efc9e62e05b8fd3dd6f953a351fd83ea59
Ofc.Diego <151096440+OfcDiego@users.noreply.github.com>
2024-08-02 19:25:27 -0500

    Actualizar nsfw-r34.js

--- plugins/nsfw-r34.js
+++ plugins/nsfw-r34.js
@@ -1,20 +1,36 @@
-import fetch from 'node-fetch'
-let handler = async (m, { conn, text, usedPrefix, command }) => {
-if (!db.data.chats[m.chat].modohorny && m.isGroup) return conn.reply(m.chat, `🚩 El grupo no admite contenido *Nsfw.*`, m, rcanal)
-if (!text) return m.reply('🚩 Ingresa el nombre de la imágen que estas buscando.')
-await m.react(rwait)
+import fetch from 'node-fetch';
+const handler = async (m, { conn, args, usedPrefix }) => {
+if (!args[0]) {
+if (!db.data.chats[m.chat].modohorny && m.isGroup) return conn.reply(m.chat, `🚩 *¡Este comando está deshabilitado en este grupo!*`, m, rcanal)
+await conn.reply(m.chat, '🚩 Ingresa el nombre de la imágen que estas buscando', m, rcanal);
+return;
+}
+const use = args[0];
+const url = `https://rule34.xxx/index.php?page=dapi&s=post&q=index&json=1&tags=${use}`;
 try {
-let { dl_url } = await Starlights.rule34(text)
-await conn.sendFile(m.chat, dl_url, 'thumbnail.jpg', `*» Resultado* : ${text}`, m, null, rcanal)
-await m.react(done)
-} catch {
-conn.reply(m.chat, '🚩 Ocurrió un error', m, fake)
-await m.react(error)
-}}
-handler.help = ['rule34 *<búsqueda>*']
-handler.tags = ['nsfw']
-handler.command = ['rule34', 'r34']
-handler.register = true 
-//handler.estrellas = 20
-//handler.group = true 
-export default handler
+conn.reply(m.chat, wait, m, {
+contextInfo: { externalAdReply :{ mediaUrl: null, mediaType: 1, showAdAttribution: true,
+title: packname,
+body: wm,
+previewType: 0, thumbnail: icons,
+sourceUrl: channel }}})
+const response = await fetch(url);
+const data = await response.json();
+if (!data || data.length === 0) {
+await conn.reply(m.chat, `🚩 No hubo resultados para *${use}*`, m, rcanal);
+return;
+}
+const randomIndex = Math.floor(Math.random() * data.length);
+const randomImage = data[randomIndex];
+const urlimg = randomImage.file_url;
+await conn.sendFile(m.chat, urlimg, 'thumbnail.jpg', `*Resultados De:* ${use}`, m, null, rcanal)
+} catch (error) {
+console.error(error);
+await m.reply('🚩 Ocurrió un error.');
+}};
+handler.help = ['r34 <texto>'];
+handler.command = ['r34', 'rule34'];
+handler.tags = ['nsfw'];
+handler.register = true;
+// handler.estrellas = 2;
+export default handler;
