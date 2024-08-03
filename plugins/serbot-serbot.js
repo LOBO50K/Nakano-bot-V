f21664e358c7792e1762421fa5e5d5be26083288
Ofc.Diego <151096440+OfcDiego@users.noreply.github.com>
2024-08-02 21:51:35 -0500

    Actualizar serbot-serbot.js

--- plugins/serbot-serbot.js
+++ plugins/serbot-serbot.js
@@ -142,7 +142,7 @@ creloadHandler(false)
 serbot()
 }
 handler.help = ["serbot"]
-handler.tags = ["jadibot"]
+handler.tags = ["serbot"]
 handler.command = ["serbot", "jadibot", "qr", "botclone"]
 // handler.register = true
 export default handler
