f6f3e8a98e03820e127e30baabf640f27edefead
Ofc.Diego <151096440+OfcDiego@users.noreply.github.com>
2024-08-02 21:54:23 -0500

    Actualizar serbot-stop.js

--- plugins/serbot-stop.js
+++ plugins/serbot-stop.js
@@ -1,5 +1,5 @@
 let handler  = async (m, { conn }) => {
-if (global.conn.user.jid == conn.user.jid) conn.reply(m.chat, `🍟 El Bot principal no se puede detener.`, m, fake)
+if (global.conn.user.jid == conn.user.jid) conn.reply(m.chat, `🍟 El Bot principal no se puede apagar.`, m, fake)
 else {
 await conn.reply(m.chat, `🚩 Adiós Ai`, m, fake)
 conn.ws.close()
