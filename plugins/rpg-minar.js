75d6d3248e7b173e8cc0df852cdcb329c28fda34
Ofc.Diego <151096440+OfcDiego@users.noreply.github.com>
2024-08-03 21:22:53 -0500

    Actualizar rpg-minar.js

--- plugins/rpg-minar.js
+++ plugins/rpg-minar.js
@@ -4,11 +4,7 @@ let handler = async (m, { conn }) => {
 let user = global.db.data.users[m.sender]
 let estrellas = `${pickRandom([20, 5, 7, 8, 88, 40, 50, 70, 90, 999, 300])}` * 1; let emerald = `${pickRandom([1, 5, 7, 8])}` * 1; let iron = `${pickRandom([5, 6, 7, 9, 10, 15, 20, 25, 30, 35, 40, 45, 50, 55, 60, 65, 70, 75, 80])}` * 1; let gold = `${pickRandom([20, 5, 7, 8, 88, 40, 50])}` * 1; let coal = `${pickRandom([20, 5, 7, 8, 88, 40, 50, 80, 70, 60, 100, 120, 600, 700, 64])}` * 1; let stone = `${pickRandom([200, 500, 700, 800, 900, 4000, 300])}` * 1
 let tiempo = 5 * 60
-if (cooldowns[m.sender] && Date.now() - cooldowns[m.sender] < tiempo * 1000) {
-let tiempo2 = segundosAHMS(Math.ceil((cooldowns[m.sender] + tiempo * 1000 - Date.now()) / 1000))
-conn.reply(m.chat, `🍟 Hola ${nombre}, Ya has minado recientemente, espera ⏱️ *${tiempo2}* para regresar a la Mina.`, m, fake)
-return
-}
+
 let hasil = Math.floor(Math.random() * 1000)
 let info = `⛏️ *Te has adentrando en lo profundo de las cuevas*
 
@@ -26,6 +22,9 @@ conn.fakeReply(m.chat, info, '0@s.whatsapp.net', '😊 *Minando.. - YaemoriBot* 
 await m.react('⛏️')
 await conn.reply(m.chat, minar, m, rcanal)
 cooldowns[m.sender] = Date.now()
+
+let time = global.db.data.users[m.sender].lastmiming + 600000
+if (new Date - global.db.data.users[m.sender].lastmiming < 600000) return conn.reply(m.chat, `*⏰ Debes esperar ${msToTime(time - new Date())} para volver a minar*`, m, fake, )
 
 user.health -= 50
 user.pickaxedurability -= 30
