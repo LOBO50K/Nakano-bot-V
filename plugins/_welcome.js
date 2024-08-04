ffee32e571c41a7d9c1fb1478de6114091b3d46d
Ofc.Diego <151096440+OfcDiego@users.noreply.github.com>
2024-08-04 10:10:09 -0500

    Actualizar _welcome.js

--- plugins/_welcome.js
+++ plugins/_welcome.js
@@ -3,22 +3,21 @@ import fetch from 'node-fetch'
 
 export async function before(m, {conn, participants, groupMetadata}) {
   if (!m.messageStubType || !m.isGroup) return !0;
-  let img = imagen1
   let chat = global.db.data.chats[m.chat]
 
   if (chat.welcome && m.messageStubType == 27) {
-    let welcome = `Bienvenido @${m.messageStubParameters[0].split`@`[0]}`
-await conn.sendMini(m.chat, packname, dev, welcome, img, img, redes, fkontak)
+    let bienvenido = `Bienvenido @${m.messageStubParameters[0].split`@`[0]}`
+await conn.sendMini(m.chat, packname, dev, bienvenido, welcome, welcome, redes, fkontak)
   }
 
   if (chat.welcome && m.messageStubType == 28) {
     let bye = `Adios @${m.messageStubParameters[0].split`@`[0]}`
-await conn.sendMini(m.chat, packname, dev, bye, img, img, redes, fkontak)
+await conn.sendMini(m.chat, packname, dev, bye, adios, adios, redes, fkontak)
   }
 
   if (chat.welcome && m.messageStubType == 32) {
     let kick = `Adios @${m.messageStubParameters[0].split`@`[0]}`
-await conn.sendMini(m.chat, packname, dev, kick, img, img, redes, fkontak)
+await conn.sendMini(m.chat, packname, dev, kick, adios, adios, redes, fkontak)
 }}
 
 /*import {WAMessageStubType} from '@whiskeysockets/baileys'
