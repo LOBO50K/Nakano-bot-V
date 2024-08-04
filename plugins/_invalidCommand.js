7c5a21467aaa3069a73290f48abcd5a44c4a6e1a
Ofc.Diego <151096440+OfcDiego@users.noreply.github.com>
2024-08-03 19:22:24 -0500

    Actualizar _invalidCommand.js

--- plugins/_invalidCommand.js
+++ plugins/_invalidCommand.js
@@ -1,22 +1,31 @@
-import didyoumean from 'didyoumean'
-import similarity from 'similarity'
-
-export async function before(m, { conn, match, usedPrefix, command }) {
-	
-if ((usedPrefix = (match[0] || '')[0])) {
-let noPrefix = m.text.replace(usedPrefix, '')
-let args = noPrefix.trim().split` `.slice(1)
-let text = args.join` `
-let help = Object.values(plugins).filter(v => v.help && !v.disabled).map(v => v.help).flat(1)
-if (help.includes(noPrefix)) return
-let mean = didyoumean(noPrefix, help)
-let sim = similarity(noPrefix, mean)
-let som = sim * 100
-let who = m.mentionedJid && m.mentionedJid[0] ? m.mentionedJid[0] : m.fromMe ? conn.user.jid : m.sender
-let name = await conn.getName(who)
-let caption = `😊 El comando no es válido.
-> Usa *!menu* para ver los comandos disponibles.`
-if (mean) conn.reply(m.chat, caption, m, { mentions: [who]})
-}
-
+export async function before(z) {
+  if (!z.text || !global.prefix.test(z.text)) {
+    return;
+  }
+  const a = global.prefix.exec(z.text)[0];
+  const b = z.text.slice(a.length).trim().split(' ')[0].toLowerCase();
+  const c = (d, e) => {
+    for (let f of Object.values(e)) {
+      if (f.command && (Array.isArray(f.command) ? f.command : [f.command]).includes(d)) {
+        return true;
+      }
+    }
+    return false;
+  };
+  if (c(b, global.plugins)) {
+    let g = global.db.data.chats[z.chat];
+    let h = global.db.data.users[z.sender];
+    if (g.isBanned) {
+      return;
+    }
+    if (!h.commands) {
+      h.commands = 0;
+    }
+    h.commands += 1;
+    await conn.sendPresenceUpdate('composing', z.chat);
+  } else {
+    const comando = z.text.trim().split(' ')[0];
+    await z.reply(`🚩 El comando *${comando}* no es válido.\nUsa */menu* para ver los comandos disponibles.`);
+    await z.react(error);
+  }
 }
