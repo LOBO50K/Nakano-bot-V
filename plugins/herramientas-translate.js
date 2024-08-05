9cfc502e53a563f09a10c67d89351b6386f7ffa3
Ofc.Diego <151096440+OfcDiego@users.noreply.github.com>
2024-08-05 12:41:47 -0500

    Actualizar herramientas-translate.js

--- plugins/herramientas-translate.js
+++ plugins/herramientas-translate.js
@@ -12,12 +12,13 @@ const handler = async (m, {args, usedPrefix, command}) => {
   }
   if (!text && m.quoted && m.quoted.text) text = m.quoted.text;
   try {
-    conn.reply(m.chat, wait, m, {
+    await m.react(rwait)
+   /* conn.reply(m.chat, wait, m, {
     contextInfo: { externalAdReply :{ mediaUrl: null, mediaType: 1, showAdAttribution: true,
     title: packname,
     body: wm,
     previewType: 0, thumbnail: icons,
-    sourceUrl: redes }}})
+    sourceUrl: redes }}}) */
     const result = await translate(`${text}`, {to: lang, autoCorrect: true});
     await m.reply('*Traducción:* ' + result.text);
   } catch {
@@ -31,9 +32,11 @@ const handler = async (m, {args, usedPrefix, command}) => {
       const lol = await fetch(`https://api.lolhuman.xyz/api/translate/auto/${lang}?apikey=${lolkeysapi}&text=${text}`);
       const loll = await lol.json();
       const result2 = loll.result.translated;
-      await m.reply('*Traducción:* ' + result2);
+      await conn.reply(m.chat, '*Traducción:* ' + result2, m, fake);
+      await m.react(done)
     } catch {
-      await m.reply('✨️ *Ocurrió Un Error*');
+      await m.react(error)
+      await conn.reply(m.chat, '✨️ *Ocurrió Un Error*', m, fake);
     }
   }
 };
