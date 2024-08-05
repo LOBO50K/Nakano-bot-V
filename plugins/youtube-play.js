62772c74a780ec22fd8e0e81e66c5aef848eb97c
Ofc.Diego <151096440+OfcDiego@users.noreply.github.com>
2024-08-04 22:01:55 -0500

    Actualizar youtube-play.js

--- plugins/youtube-play.js
+++ plugins/youtube-play.js
@@ -3,23 +3,17 @@ import yts from "yt-search"
 
 let handler = async (m, { conn, command, args, text, usedPrefix }) => {
 if (!text) return conn.reply(m.chat, `🚩 *Ingrese el nombre de un video de YouTube*\n\nEjemplo, !${command} Distancia - Kimberly Contreraxx`,  m, rcanal, )
-conn.reply(m.chat, global.wait, m, {
-contextInfo: { externalAdReply :{ mediaUrl: null, mediaType: 1, showAdAttribution: true,
-title: packname,
-body: wm,
-previewType: 0, thumbnail: icons,
-sourceUrl: channel }}})
 try {
 await m.react(rwait)
 let yt_play = await search(args.join(" "))
 let img = await (await fetch(`${yt_play[0].image}`)).buffer()
 
 let txt = `*乂  Y O U T U B E  -  P L A Y  乂*\n\n`
-       txt += `✩ *𝐓𝐢𝐭𝐮𝐥𝐨:*\n${yt_play[0].title}\n\n`
-       txt += `✩ *𝐃𝐮𝐫𝐚𝐜𝐢𝐨𝐧:*\n${secondString(yt_play[0].duration.seconds)}\n\n`
-       txt += `✩ *𝐏𝐮𝐛𝐥𝐢𝐜𝐚𝐝𝐨 𝐄𝐧:*\n${yt_play[0].ago}\n\n`
-       txt += `✩ *𝐄𝐧𝐥𝐚𝐜𝐞:*\n${'https://youtu.be/' + yt_play[0].videoId}\n\n`
-       txt += `✨️ *Nota:* Para descargar responde a este mensaje con *1* o *2*.\n\n`
+       txt += `🍟 *Titulo:*\n${yt_play[0].title}\n\n`
+       txt += `⏱️ *Duración:*\n${secondString(yt_play[0].duration.seconds)}\n\n`
+       txt += `✨️ *Publicado en:*\n${yt_play[0].ago}\n\n`
+       txt += `🔗 *Enlace:*\n${'https://youtu.be/' + yt_play[0].videoId}\n\n`
+       txt += `🍭 *Nota:* Para descargar responde a este mensaje con *1* o *2*.\n\n`
        txt += `*1:* Video\n*2:* Audio`
 
 /* let txt = `・₊✧★。..・✫・🎸🎧°⋆♡₊˚ 🔮
@@ -50,22 +44,20 @@ forwardingScore: 9999,
 isForwarded: true, 
 externalAdReply: {
 title: `${yt_play[0].title}`,
-body: dev,
+body: wm,
 thumbnailUrl: img,
 thumbnail: img,
 sourceUrl: `${yt_play[0].url}`,
 mediaType: 1,
 renderLargerThumbnail: true
-}}}, { quoted: fkontak})
+}}}, { quoted: m})
 await m.react(done)
 } catch {
-await m.react(error)
 await m.reply(`✘ Ocurrío un error`)}}
 
 handler.help = ['play *<búsqueda>*', 'play2 *<busqueda>*']
 handler.tags = ['descargas', 'youtube']
 handler.command = ['play', 'play2']
-handler.register = true
 export default handler
 
 async function search(query, options = {}) {
