5a66116b974a5b0a9d4e7a3743f4019b95770b53
Ofc.Diego <151096440+OfcDiego@users.noreply.github.com>
2024-08-06 11:21:30 -0500

    Actualizar plugins

---
+++ plugins/mods-backup.js
@@ -0,0 +1,25 @@
+import fs from 'fs'
+
+let handler = async (m, { conn, text, usedPrefix, command }) => {
+await m.reply(`❮🪐❯ » *Enviando base de datos de ${wm}...*`)
+try {
+await m.react(rwait)
+let d = new Date
+let date = d.toLocaleDateString('es', { day: 'numeric', month: 'long', year: 'numeric' })
+let database = await fs.readFileSync(`./database.json`)
+let creds = await fs.readFileSync(`./MiniSession/creds.json`)
+await conn.reply(m.sender, `*• Fecha:* ${date}`, fkontak)
+await conn.sendMessage(m.sender, {document: database, mimetype: 'application/json', fileName: `database.json`}, { quoted: fkontak })
+await m.react(done)
+await conn.sendMessage(m.sender, {document: creds, mimetype: 'application/json', fileName: `creds.json`}, { quoted: fkontak })
+await m.react(done)
+} catch {
+await m.react(error)
+conn.reply(m.chat, `❮❌️❯ » *Ocurrió un error.*`, m, fake)}
+
+handler.help = ['copia']
+handler.tags = ['mods']
+handler.command = ['backup', 'respaldo', 'copia']
+handler.mods = true
+
+export default handler
