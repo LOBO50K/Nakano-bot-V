5f0cfe74e8f6cbf6c3f1e21ab86f242e5a532da0
Ofc.Diego <151096440+OfcDiego@users.noreply.github.com>
2024-08-06 11:23:07 -0500

    Actualizar mods-backup.js

--- plugins/mods-backup.js
+++ plugins/mods-backup.js
@@ -8,14 +8,14 @@ let d = new Date
 let date = d.toLocaleDateString('es', { day: 'numeric', month: 'long', year: 'numeric' })
 let database = await fs.readFileSync(`./database.json`)
 let creds = await fs.readFileSync(`./MiniSession/creds.json`)
-await conn.reply(m.sender, `*• Fecha:* ${date}`, fkontak)
+await conn.reply(m.chat, `*• Fecha:* ${date}`, m, fake)
 await conn.sendMessage(m.sender, {document: database, mimetype: 'application/json', fileName: `database.json`}, { quoted: fkontak })
 await m.react(done)
 await conn.sendMessage(m.sender, {document: creds, mimetype: 'application/json', fileName: `creds.json`}, { quoted: fkontak })
 await m.react(done)
 } catch {
 await m.react(error)
-conn.reply(m.chat, `❮❌️❯ » *Ocurrió un error.*`, m, fake)}
+conn.reply(m.chat, `❮❌️❯ » *Ocurrió un error.*`, m, fake)}}
 
 handler.help = ['copia']
 handler.tags = ['mods']
