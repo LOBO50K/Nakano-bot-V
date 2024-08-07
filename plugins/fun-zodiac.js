82f4b7ad6bf9750d28b94a0b9e2238a51f0203aa
Ofc.Diego <151096440+OfcDiego@users.noreply.github.com>
2024-08-07 15:31:07 -0500

    Actualizar fun-zodiac.js

--- plugins/fun-zodiac.js
+++ plugins/fun-zodiac.js
@@ -25,7 +25,7 @@ Signo zodical : ${zodiac}
 handler.help = ['zodiac *2002 02 25*']
 handler.tags = ['fun']
 
-handler.command = /^zodia[kc]$/i
+handler.command = ['zodiac']
 
 export default handler
 
