e07f730170b2627591ea940ca6c3813cb9b25a03
Ofc.Diego <151096440+OfcDiego@users.noreply.github.com>
2024-08-07 11:29:18 -0500

    Actualizar serbot-serbot.js

--- plugins/serbot-serbot.js
+++ plugins/serbot-serbot.js
@@ -1,250 +1,151 @@
-const { useMultiFileAuthState, DisconnectReason, makeCacheableSignalKeyStore, fetchLatestBaileysVersion} = (await import('@whiskeysockets/baileys'))
+import { DisconnectReason, useMultiFileAuthState, fetchLatestBaileysVersion, makeCacheableSignalKeyStore, jidNormalizedUser } from '@whiskeysockets/baileys'
 import qrcode from 'qrcode'
-import NodeCache from 'node-cache'
 import fs from 'fs'
-import path from 'path'
 import pino from 'pino'
-import util from 'util' 
-import * as ws from 'ws'
-const { child, spawn, exec } = await import('child_process')
-const { CONNECTING } = ws
+import crypto from 'crypto'
+import NodeCache from 'node-cache'
 import { makeWASocket } from '../lib/simple.js'
-
-let check1 = 'NjBhZGVmZWI4N2M2'
-let check2 = 'ZThkMmNkOGVlMDFmZD'
-let check3 = 'UzYTI1MTQgIGluZ'
-let check4 = 'm8tZG9uYXIuanMK'
-let check5 = 'NzZjM2ZmMzU2MTEyMzM3OTczOWU5ZmFmMDZjYzUzO'
-let check6 = 'DcgIF9hdXRvcmVzcG9uZGVyLmpzCjU5Yzc0ZjFjNmEz'
-let check8 = 'NjNmYmJjYzA1YmFiY2MzZGU4MGRlICBpbmZvLWJvdC5qcwo'
-
-let crm1 = 'Y2QgcGx1Z2lucy'
-let crm2 = 'A7IG1kNXN1b'
-let crm3 = 'SBpbmZvLWRvbmFyLmpz'
-let crm4 = 'IF9hdXRvcmVzcG9uZGVyLmpzIGluZm8tYm90Lmpz'
-let drm1 = ''
-let drm2 = ''
-let rtx = `QR`
-let rtx2 = `CODE`
-
-if (global.conns instanceof Array) console.log()
-else global.conns = []
-
-let handler = async (m, { conn, args, usedPrefix, command, isOwner }) => {
-let parentw = conn
-if (conn.user.jid !== global.conn.user.jid) return parentw.reply(m.chat, `🚩 *Diríjase al número principal del bot*\nwa.me/${global.conn.user.jid.split`@`[0]}&text=${usedPrefix + command}`, m) 
-const mcode = args[0] && args[0].includes('--code') ? true : args[1] && args[1].includes('--code') ? true : false
-
-
-let who = m.mentionedJid && m.mentionedJid[0] ? m.mentionedJid[0] : m.fromMe ? parentw.user.jid : m.sender
-let id = `${who.split`@`[0]}`
-if (mcode) {
-args[0] = args[0].replace('--code', '').trim()
-if (args[1]) args[1] = args[1].replace('--code', '').trim()
-if (args[0] == '') args[0] = undefined
-console.log(args[0])}
-if (!fs.existsSync('./jadibts/'+ id)){
-fs.mkdirSync('./jadibts/'+ id, { recursive: true })}
-args[0] && args[0] != undefined ? fs.writeFileSync('./jadibts/' + id + '/creds.json', JSON.stringify(JSON.parse(Buffer.from(args[0], 'base64').toString('utf-8')), null, '\t')) : ''
-
-if (fs.existsSync('./jadibts/' + id + '/creds.json')) {
-let creds = JSON.parse(fs.readFileSync("./jadibts/" + id + "/creds.json"))
-if (creds) {
-if (creds.registered = false) {
-fs.unlinkSync('./jadibts/' + id + '/creds.json')
-}}}
-
-const comb = Buffer.from(crm1 + crm2 + crm3 + crm4, 'base64')
-exec(comb.toString('utf-8'), async (err, stdout, stderr) => {
-const drmer = Buffer.from(drm1 + drm2, `base64`)
-async function jddt() {
-let who = m.mentionedJid && m.mentionedJid[0] ? m.mentionedJid[0] : m.fromMe ? parentw.user.jid : m.sender
-let id = `${who.split`@`[0]}`
-if (!fs.existsSync('./jadibts/'+ id)){
-fs.mkdirSync('./jadibts/'+ id, { recursive: true })
+if (global.conns instanceof Array) {
+console.log()
+} else {
+global.conns = []
 }
-args[0] ? fs.writeFileSync('./jadibts/' + id + '/creds.json', JSON.stringify(JSON.parse(Buffer.from(args[0], 'base64').toString('utf-8')), null, `\t`)) : ''
-
-let { version, isLatest } = await fetchLatestBaileysVersion()
-const msgRetry = (MessageRetryMap) => { }
-const msgRetryCache = new NodeCache()
-const { state, saveState, saveCreds } = await useMultiFileAuthState("./jadibts/" + id)
-
+let handler = async (m, { conn, args, usedPrefix, command, isOwner, isPrems, isROwner }) => {
+if (!global.db.data.settings[conn.user.jid].jadibotmd && !isROwner) {
+conn.reply(m.chat, '🚩 Este Comando está deshabilitado por mi creador.', m, rcanal)
+return
+}
+let parentw = args[0] && args[0] == "plz" ? conn : await global.conn
+if (!(args[0] && args[0] == 'plz' || (await global.conn).user.jid == conn.user.jid)) {
+return conn.reply(m.chat, `「💭」Solo puedes usar este comando en el bot principal.\n\n• Wa.me/${global.conn.user.jid.split`@`[0]}?text=${usedPrefix + command}`, m, rcanal)
+}
+async function serbot() {
+let serbotFolder = crypto.randomBytes(10).toString('hex').slice(0, 8)
+let folderSub = `./MiniJadiBot/${serbotFolder}`
+if (!fs.existsSync(folderSub)) {
+fs.mkdirSync(folderSub, { recursive: true })
+}
+if (args[0]) {
+fs.writeFileSync(`${folderSub}/creds.json`, Buffer.from(args[0], 'base64').toString('utf-8'))
+}
+const { state, saveCreds } = await useMultiFileAuthState(folderSub)
+const msgRetryCounterCache = new NodeCache()
+const { version } = await fetchLatestBaileysVersion()
 const connectionOptions = {
-printQRInTerminal: false,
 logger: pino({ level: 'silent' }),
-auth: { creds: state.creds, keys: makeCacheableSignalKeyStore(state.keys, pino({level: 'silent'})) },
-msgRetry,
-msgRetryCache,
-version,
-syncFullHistory: true,
-browser: mcode ? ['Ubuntu', 'Chrome', '110.0.5585.95'] : ['CuriosityBot', 'Opera', '5.0'],
-defaultQueryTimeoutMs: undefined,
-getMessage: async (key) => {
-if (store) {
-const msg = store.loadMessage(key.remoteJid, key.id)
-return msg.message && undefined
-} return {
-conversation: 'botBot-MD',
-}}}
-
+printQRInTerminal: true,
+browser: ['Yaemori Sub-Bot', 'Edge', '2.0.0'],
+auth: {
+creds: state.creds,
+keys: makeCacheableSignalKeyStore(state.keys, pino({ level: "fatal" }).child({ level: "fatal" })),
+},
+markOnlineOnConnect: true,
+generateHighQualityLinkPreview: true,
+getMessage: async (clave) => {
+let jid = jidNormalizedUser(clave.remoteJid)
+let msg = await store.loadMessage(jid, clave.id)
+return msg?.message || ""
+},
+msgRetryCounterCache,
+version
+}
 let conn = makeWASocket(connectionOptions)
 conn.isInit = false
 let isInit = true
-
 async function connectionUpdate(update) {
 const { connection, lastDisconnect, isNewLogin, qr } = update
-if (isNewLogin) conn.isInit = false
-if (qr && !mcode) return parentw.sendMessage(m.chat, { image: await qrcode.toBuffer(qr, { scale: 8 }), caption: rtx + drmer.toString('utf-8')}, { quoted: m})
-if (qr && mcode) {
-parentw.sendMessage(m.chat, {text : rtx2 + drmer.toString('utf-8')}, { quoted: m })
-await sleep(5000)
-let secret = await conn.requestPairingCode((m.sender.split`@`[0]))
-await m.reply(secret)}
-const code = lastDisconnect?.error?.output?.statusCode || lastDisconnect?.error?.output?.payload?.statusCode
-console.log(code)
-const endSesion = async (loaded) => {
-if (!loaded) {
-try {
-conn.ws.close()
-} catch {
+if (isNewLogin) {
+conn.isInit = true
 }
-conn.ev.removeAllListeners()
-let i = global.conns.indexOf(conn)                
-if (i < 0) return 
+if (qr) {
+let txt = '🚩 S E R B O T - S U B B O T 🚩\n\n*Escanea este QR para ser un Sub Bot*\n\n🍟 Pasos para escanear:\n\n`1` : Haga click en los 3 puntos\n\n`2` : Toque dispositivos vinculados\n\n`3` : Escanea este QR\n\n> *Nota:* Este código QR expira en 30 segundos.'
+
+let sendQR = await parentw.sendFile(m.chat, await qrcode.toDataURL(qr, { scale: 8 }), "qrcode.png", txt, m, null, rcanal)
+setTimeout(() => {
+parentw.sendMessage(m.chat, { delete: sendQR.key })
+}, 30000)
+}
+const code = lastDisconnect?.error?.output?.statusCode || lastDisconnect?.error?.output?.payload?.statusCode
+if (code && code !== DisconnectReason.loggedOut && conn?.ws.socket == null) {
+let i = global.conns.indexOf(conn)
+if (i < 0) {
+return console.log(await creloadHandler(true).catch(console.error))
+}
 delete global.conns[i]
 global.conns.splice(i, 1)
-}}
-
-const reason = lastDisconnect?.error?.output?.statusCode || lastDisconnect?.error?.output?.payload?.statusCode
-if (connection === 'close') {
-console.log(reason)
-if (reason == 405) {
-await fs.unlinkSync('./jadibts/' + id + '/creds.json')
-
-return await conn.reply(m.chat, '🚩 *Conexión cerrada*', m)
+if (code !== DisconnectReason.connectionClosed) {
+await parentw.reply(conn.user.jid, "🚩 Conexión perdida con el servidor.", m)
 }
-if (reason === DisconnectReason.restartRequired) {
-jddt()
-return console.log('🎌 Conexión reemplazada, se ha abierto otra nueva sesion, por favor, cierra la sesión actual primero')
-} else if (reason === DisconnectReason.loggedOut) {
-sleep(4000)
-return conn.reply(m.chat, '🎌 *La conexión se ha cerrado, tendras que volver a conectarse usando:*\n!deletesesion (Para borrar los datos y poder volver a solitar el QR o el código de emparejamiento', m)
-} else if (reason == 428) {
-await endSesion(false)
-return conn.reply(m.chat, '🎌 *La conexión se ha cerrado de manera inesperada, intentaremos reconectar...*', m)
-} else if (reason === DisconnectReason.connectionLost) {
-await jddt()
-return console.log('🎌 Conexión perdida con el servidor, reconectando')
-} else if (reason === DisconnectReason.badSession) {
-return await conn.reply(m.chat, '🚩 *La conexión se ha cerrado, deberá de conectarse manualmente*', m)
-} else if (reason === DisconnectReason.timedOut) {
-await endSesion(false)
-return console.log('🚩 Tiempo de conexión agotado, reconectando....')
-} else {
-console.log('🚩 Razon de desconexión desconocida: ${reason || ""} >> ${connection || ""}')
-}}
-if (global.db.data == null) loadDatabase()
-if (connection == `open`) {
+}
+if (global.db.data == null) {
+loadDatabase()
+}
+if (connection == "open") {
 conn.isInit = true
 global.conns.push(conn)
-await parentw.sendMessage(m.chat, {text : args[0] ? `✅ *Conectado*` : `🎌 *Conectado*\n\nUtilice su ID para volver a conectarse`}, { quoted: m })
-await parentw.sendMessage(m.chat, {text : `🎌 *Esta conectado, espere un momento*`}, { quoted: m })
+await parentw.reply(m.chat, args[0] ? '🐢 Conectado con éxito al WhatsApp.' : '🚩 Vinculaste un Sub-Bot con éxito.', m, rcanal)
 await sleep(5000)
-if (!args[0]) parentw.sendMessage(m.chat, {text : usedPrefix + command + ' ' + Buffer.from(fs.readFileSync('./jadibts/' + id + '/creds.json'), 'utf-8').toString('base64')}, { quoted: m })    
-
-}}
-setInterval(async () => {
-if (!conn.user) {
-try { conn.ws.close() } catch (e) {      
-console.log(await creloadHandler(true).catch(console.error))
+if (args[0]) {
+return
 }
+await parentw.reply(conn.user.jid, `🚩 *Para volver a vincular un sub Bot use su token*`, m, rcanal)
+}
+}
+const timeoutId = setTimeout(() => {
+if (!conn.user) {
+try {
+conn.ws.close()
+} catch {}
 conn.ev.removeAllListeners()
-let i = global.conns.indexOf(conn)                
-if (i < 0) return
+let i = global.conns.indexOf(conn)
+if (i >= 0) {
 delete global.conns[i]
 global.conns.splice(i, 1)
-}}, 60000)
-
-let handler = await import('../handler.js')
+}
+fs.rmdirSync(`./MiniJadiBot/${serbotFolder}`, { recursive: true })
+}
+}, 30000)
+let handler = await import("../handler.js")
 let creloadHandler = async function (restatConn) {
 try {
 const Handler = await import(`../handler.js?update=${Date.now()}`).catch(console.error)
-if (Object.keys(Handler || {}).length) handler = Handler
-
+if (Object.keys(Handler || {}).length) {
+handler = Handler
+}
 } catch (e) {
 console.error(e)
 }
 if (restatConn) {
-const oldChats = conn.chats
-try { conn.ws.close() } catch { }
+try {
+conn.ws.close()
+} catch {}
 conn.ev.removeAllListeners()
-conn = makeWASocket(connectionOptions, { chats: oldChats })
+conn = makeWASocket(connectionOptions)
 isInit = true
 }
 if (!isInit) {
-conn.ev.off('messages.upsert', conn.handler)
-conn.ev.off('group-participants.update', conn.participantsUpdate)
-conn.ev.off('groups.update', conn.groupsUpdate)
-conn.ev.off('message.delete', conn.onDelete)
-conn.ev.off('call', conn.onCall)
-conn.ev.off('connection.update', conn.connectionUpdate)
+conn.ev.off("messages.upsert", conn.handler)
+conn.ev.off("connection.update", conn.connectionUpdate)
 conn.ev.off('creds.update', conn.credsUpdate)
-}
-conn.welcome = 'HOLA'
-conn.bye = 'ADIOS GAY'
-conn.sdemote = '*@user* ¡Abandona el grupo!'
-conn.sDesc = '¡Se ha modificado la descripción!\n\n*Nueva descripción:* @desc'
-conn.sSubject = '¡Se ha modificado el título del grupo!'
-conn.sIcon = '¡Se ha cambiado la foto del grupo!'
-conn.sRevoke = '¡Se ha actualizado el enlace del grupo!*\n*Nuevo enlace:* @revoke'
-
+} 
 conn.handler = handler.handler.bind(conn)
-conn.participantsUpdate = handler.participantsUpdate.bind(conn)
-conn.groupsUpdate = handler.groupsUpdate.bind(conn)
-conn.onDelete = handler.deleteUpdate.bind(conn)
-conn.onCall = handler.callUpdate.bind(conn)
 conn.connectionUpdate = connectionUpdate.bind(conn)
 conn.credsUpdate = saveCreds.bind(conn, true)
-
-const currentDateTime = new Date()
-const messageDateTime = new Date(conn.ev * 1000)
-if (currentDateTime.getTime() - messageDateTime.getTime() <= 300000) {
-console.log('Leyendo mensaje entrante:', conn.ev)
-Object.keys(conn.chats).forEach(jid => {
-conn.chats[jid].isBanned = false
-})
-} else {
-console.log(conn.chats, `🚩 Omitiendo mensajes en espera.`, conn.ev)
-Object.keys(conn.chats).forEach(jid => {
-conn.chats[jid].isBanned = true
-})
-}
-
-conn.ev.on(`messages.upsert`, conn.handler)
-conn.ev.on(`group-participants.update`, conn.participantsUpdate)
-conn.ev.on(`groups.update`, conn.groupsUpdate)
-conn.ev.on(`message.delete`, conn.onDelete)
-conn.ev.on(`call`, conn.onCall)
-conn.ev.on(`connection.update`, conn.connectionUpdate)
-conn.ev.on(`creds.update`, conn.credsUpdate)
+conn.ev.on("messages.upsert", conn.handler)
+conn.ev.on("connection.update", conn.connectionUpdate)
+conn.ev.on("creds.update", conn.credsUpdate)
 isInit = false
 return true
 }
 creloadHandler(false)
 }
-jddt()
-})
-
-} 
-handler.help = [`jadibot`, `serbot`, `getcode`, `rentbot`]
-handler.tags = [`jadibot`]
-handler.command = ['jadibot', 'serbot', 'rentbot']
-
-handler.private = false
-
+serbot()
+}
+handler.help = ["serbot"]
+handler.tags = ["serbot"]
+handler.command = ["serbot", "jadibot", "qr", "botclone"]
+// handler.register = true
 export default handler
-
-const delay = (ms) => new Promise((resolve) => setTimeout(resolve, ms))
 function sleep(ms) {
-return new Promise(resolve => setTimeout(resolve, ms));}
+return new Promise(resolve => setTimeout(resolve, ms))
+}
