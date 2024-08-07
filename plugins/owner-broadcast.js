bc751674e84667caf7cb71a6fcc33e9cdae40b5d
Ofc.Diego <151096440+OfcDiego@users.noreply.github.com>
2024-08-07 09:40:17 -0500

    Actualizar owner-broadcast.js

--- plugins/owner-broadcast.js
+++ plugins/owner-broadcast.js
@@ -3,13 +3,13 @@
 import {randomBytes} from 'crypto';
 
 const handler = async (m, {conn, command, participants, usedPrefix, text}) => {
-  if (!text) return '*Y EL TEXTO?*';
+  if (!text) return '🚩 Te faltó el texto que quieres transmitir a todos los chats.';
   const fkontak = {'key': {'participants': '0@s.whatsapp.net', 'remoteJid': 'status@broadcast', 'fromMe': false, 'id': 'Halo'}, 'message': {'contactMessage': {'vcard': `BEGIN:VCARD\nVERSION:3.0\nN:Sy;Bot;;;\nFN:y\nitem1.TEL;waid=${conn.user.jid.split('@')[0]}:${conn.user.jid.split('@')[0]}\nitem1.X-ABLabel:Ponsel\nEND:VCARD`}}, 'participant': '0@s.whatsapp.net'};
   const cc4 = text ? m : m.quoted ? await m.getQuotedObj() : false || m;
   const teks4 = text ? text : cc4.text;
   const groups2 = Object.keys(await conn.groupFetchAllParticipating());
   const chats2 = Object.keys(global.db.data.users).filter((user) => user.endsWith('@s.whatsapp.net'));
-  await conn.reply(m.chat, '*🛑 Enviando A Todos Los Chats*', m);
+  await conn.reply(m.chat, '⭐️ *El texto se está enviando a los chats*', m, fake);
   const start2 = new Date().getTime();
   const usersTag2 = participants.map((u) => conn.decodeJid(u.id));
   let totalPri2 = 0;
@@ -17,12 +17,12 @@ const handler = async (m, {conn, command, participants, usedPrefix, text}) => {
     const group = groups2[i];
     const delay = i * 4000; // 4 seg
     setTimeout(async () => {
-      await conn.reply(group, `*☁️ 𝖢𝗈𝗆𝗎𝗇𝗂𝖼𝖺𝖽𝗈 ☁️*\n\n` + teks4, {mentions: usersTag2}, {quoted: fkontak});
+      await conn.reply(group, `⭐️ M E N S A J E ⭐️\n\n` + teks4, {mentions: usersTag2}, {quoted: fkontak});
     }, delay);
   }
   for (const user of chats2) {
     await new Promise((resolve) => setTimeout(resolve, 2000)); // 2 segundos
-    await conn.reply(user, `*☁️ 𝖢𝗈𝗆𝗎𝗇𝗂𝖼𝖺𝖽𝗈 ☁️*\n\n` + teks4, fkontak, null);
+    await conn.reply(user, `⭐️ M E N S A J E ⭐️\n\n` + teks4, fkontak, null);
     totalPri2++;
     if (totalPri2 >= 500000) {
       break;
@@ -40,12 +40,10 @@ const handler = async (m, {conn, command, participants, usedPrefix, text}) => {
   } else {
     time2 = `${time2} segundos`;
   }
-  await m.reply(`𝖬𝖾𝗇𝗌𝖺𝗃𝖾 𝖤𝗇𝗏𝗂𝖺𝖽𝗈 𝖠:
-\`\`\`𝗖𝗵𝗮𝘁 𝗣𝗿𝗶𝘃𝗮𝗱𝗼𝘀 >> ${totalPrivate2}\`\`\`
-\`\`\`𝗖𝗵𝗮𝘁 𝗚𝗿𝘂𝗽𝗼𝘀  >>   ${totalGroups2}\`\`\`
-\`\`\`𝙲𝙷𝙰𝚃𝚂 𝚃𝙾𝚃𝙰𝙻𝙴𝚂  >>   ${total2}\`\`\`\n\n*𝖳𝗂𝖾𝗆𝗉𝗈 𝖳𝗈𝗍𝖺𝗅 𝖣𝖾 𝖤𝗇𝗏𝗂𝗈 ${time2}*\n${totalPri2 >= 500000 ? `\n*☁️ Bot - Yotsuba*` : ''}`);
+await m.reply(`⭐️ Mensaje enviado a:\n🍟 Chats Privados: ${totalPrivate2}\n⚜️ Chats De Grupos: ${totalGroups2}\n🚩 Chats Totales: ${total2}\n\n⏱️ *Tiempo total de envio:* ${time2}
+${totalPri2 >= 500000 ? `\n${wm}` : ''}`);
 };
-handler.help = ['broadcast', 'bc'].map((v) => v + ' <teks>');
+handler.help = ['broadcast', 'bc'];
 handler.tags = ['owner'];
 handler.command = ['bc', 'comunicado'];
 
