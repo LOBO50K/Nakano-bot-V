f9e0754a4d2ce2b21995e55450d8c5ec7eadf478
Ofc.Diego <151096440+OfcDiego@users.noreply.github.com>
2024-08-08 11:19:47 -0500

    Actualizar fun-love2.js

--- plugins/fun-love2.js
+++ plugins/fun-love2.js
@@ -7,7 +7,7 @@ let handler = async (m, { conn, usedPrefix }) => {
     let who;
     if (m.isGroup) who = m.mentionedJid[0] ? m.mentionedJid[0] : m.quoted ? m.quoted.sender : false;
     else who = m.chat;
-    if (!who) conn.reply(m.chat, '❕️ Etiqueta o menciona a alguien', m, rcanal);
+    if (!who) throw '❕️ Etiqueta o menciona a alguien';
 
     let user = global.db.data.users[who];
     let name = conn.getName(m.sender);
