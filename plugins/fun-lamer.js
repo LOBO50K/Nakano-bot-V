665c2535f3e064f28eca76eefff9895801ed8c80
David Chian <107944279+David-Chian@users.noreply.github.com>
2024-08-08 08:56:08 -0400

    Actualizar fun-lamer.js

--- plugins/fun-lamer.js
+++ plugins/fun-lamer.js
@@ -33,7 +33,7 @@ let handler = async (m, { conn, usedPrefix }) => {
 }
 
 handler.help = ['lamber @tag'];
-handler.tags = ['reaccion'];
+handler.tags = ['fun'];
 handler.command = ['lick','lamer','lamber'];
 handler.group = true;
 
