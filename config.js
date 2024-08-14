38b0a22fd2231333a72b58042254e84b2a324137
DarkesJs <theyaemoribot@gmail.com>
2024-08-13 21:37:17 -0500

    Actualizar config.js

--- config.js
+++ config.js
@@ -2,7 +2,7 @@ import { watchFile, unwatchFile } from 'fs'
 import chalk from 'chalk'
 import { fileURLToPath } from 'url'
 import fs from 'fs'
-import cheerio from 'cheerio'
+//import cheerio from 'cheerio'
 import fetch from 'node-fetch'
 import axios from 'axios'
 import moment from 'moment-timezone' 
@@ -56,7 +56,7 @@ global.estilo = { key: {  fromMe: false, participant: `0@s.whatsapp.net`, ...(fa
 
 //*─ׄ─ׅ─ׄ─⭒─ׄ─ׅ─ׄ─⭒─ׄ─ׅ─ׄ─⭒─ׄ─ׅ─ׄ─⭒─ׄ─ׅ─ׄ─⭒─ׄ─ׅ─ׄ─*
 
-global.cheerio = cheerio
+//global.cheerio = cheerio
 global.fs = fs
 global.fetch = fetch
 global.axios = axios
