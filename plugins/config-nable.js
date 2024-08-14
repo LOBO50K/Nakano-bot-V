0e598a8c4b8b46b062d2b1f726521abe5a17b7f1
DarkesJs <theyaemoribot@gmail.com>
2024-08-14 09:46:00 -0500

    Actualizar config-nable.js

--- plugins/config-nable.js
+++ plugins/config-nable.js
@@ -48,7 +48,7 @@ let handler = async (m, { conn, usedPrefix, command, args, isOwner, isAdmin, isR
         global.dfail('admin', m, conn)
         throw false
       }
-      bot.autoread = isEnable
+      bot.autoread2 = isEnable
       break
 
   case 'antiver':
