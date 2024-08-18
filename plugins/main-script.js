3e3b2fc4f7f90e43ec706582f5198211cbd7279f
Ofc.Diego <151096440+OfcDiego@users.noreply.github.com>
2024-08-18 15:17:09 -0500

    Actualizar main-script.js

--- plugins/main-script.js
+++ plugins/main-script.js
@@ -19,10 +19,10 @@ let img = imagen1
 
 //await conn.sendMini(m.chat, packname, wm, txt, img, img, redes, fkontak)
 const prep = generateWAMessageFromContent(m.chat, { "orderMessage": { "orderId":"6288215463787", "itemCount": 2022, "message": txt, "orderTitle": botname, "footerText": "Yaemori Bot - MD", "token": "AR6xBKbXZn0Xwmu76Ksyd7rnxI+Rx87HfinVlW4lwXa6JA==", "thumbnail": img, "surface": "CATALOG" } }, { quoted: fkontak })
-await conn.relayMessage(m.chat, prep.message,  { messageId: prep.key.id })
-} catch {
+await conn.relayMessage(m.chat, prep.message,  { messageId: prep.key.id })}
+/*} catch {
 await m.react(error)
-}}
+}}*/
 handler.help = ['script']
 handler.tags = ['main']
 handler.command = ['script', 'sc']
