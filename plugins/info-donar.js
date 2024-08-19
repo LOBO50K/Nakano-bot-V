a2438b8b7174b6473387b0dad8bfb6d51b2ce569
Ofc.Diego <151096440+OfcDiego@users.noreply.github.com>
2024-08-19 13:22:00 -0500

    Actualizar plugins

---
+++ plugins/info-donar.js
@@ -0,0 +1,31 @@
+let handler = async (m, { conn, command }) => {
+let paypal = 'https://paypal.me/Corinplushost'
+let fkontak = { "key": { "participants":"0@s.whatsapp.net", "remoteJid": "status@broadcast", "fromMe": false, "id": "Halo" }, "message": { "contactMessage": { "vcard": `BEGIN:VCARD\nVERSION:3.0\nN:Sy;Bot;;;\nFN:y\nitem1.TEL;waid=${m.sender.split('@')[0]}:${m.sender.split('@')[0]}\nitem1.X-ABLabel:Ponsel\nEND:VCARD` }}, "participant": "0@s.whatsapp.net" }
+  let user = db.data.users[m.sender]
+let str = `💖 𝙃𝙤𝙡𝙖!! 𝘼𝙜𝙧𝙖𝙙𝙚𝙯𝙘𝙤 𝙨𝙞 𝙢𝙚 𝘼𝙥𝙤𝙮𝙖𝙨 𝘿𝙤𝙣𝙖𝙣𝙙𝙤. 🎁 𝙈𝙚 𝙖𝙜𝙧𝙖𝙙𝙖 𝙚𝙡 𝙏𝙧𝙖𝙗𝙖𝙟𝙤 𝙦𝙪𝙚 𝙝𝙚 𝙇𝙤𝙜𝙧𝙖𝙙𝙤 𝙮 𝙡𝙤 𝘾𝙤𝙢𝙥𝙖𝙧𝙩𝙤 𝙘𝙤𝙣 𝙐𝙨𝙩𝙚𝙙𝙚𝙨. 𝙂𝙧𝙖𝙘𝙞𝙖𝙨!
+┈┈┈┈┈┈┈┈┈┈┈┈┈┈┈┈┈
+_*Paypal OfcBot*_
+*${paypal}*
+┈┈┈┈┈┈┈┈┈┈┈┈┈┈┈┈┈`
+await conn.sendMessage(m.chat, { 
+text: str, 
+contextInfo:{
+forwardingScore: 9999999,
+isForwarded: true, 
+mentionedJid:[m.sender],
+"externalAdReply": {
+"showAdAttribution": true,
+"renderLargerThumbnail": true,
+"thumbnail": imagen1, 
+"title": `${botname}`, 
+"containsAutoReply": true,
+"mediaType": 1, 
+"mediaUrl": paypal, 
+"sourceUrl": paypal, 
+}
+}
+}, { quoted: fkontak })}
+handler.help = ['donar']
+handler.tags = ['info']
+handler.command = ['donar', 'apoyar']
+export default handler
