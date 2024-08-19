d03827eb0a4657dd9b07d3eed7872a0ae3840677
Ofc.Diego <151096440+OfcDiego@users.noreply.github.com>
2024-08-19 13:17:55 -0500

    Actualizar info-creador.js

--- plugins/info-creador.js
+++ plugins/info-creador.js
@@ -1,28 +1,14 @@
-var handler  = async (m, { conn, command, args, usedPrefix, DevMode }) => {
-let fkontak = { "key": { "participants":"0@s.whatsapp.net", "remoteJid": "status@broadcast", "fromMe": false, "id": "Halo" }, "message": { "contactMessage": { "vcard": `BEGIN:VCARD\nVERSION:3.0\nN:Sy;Bot;;;\nFN:y\nitem1.TEL;waid=${m.sender.split('@')[0]}:${m.sender.split('@')[0]}\nitem1.X-ABLabel:Ponsel\nEND:VCARD` }}, "participant": "0@s.whatsapp.net" }        
+function handler(m) {
+  const data = global.owner.filter(([id, isCreator]) => id && isCreator)
+  this.sendContact(m.chat, data.map(([id, name]) => [id, name]), estilo, { contextInfo: { externalAdReply: { showAdAttribution: true }}})
 
-const owner = `👑 *Propietario:*
-${creador}
-༺═──────────────═༻
-🍧 *Youtube:*
-${yt}
-༺═──────────────═༻
-🍭 *Bot Oficial:*
-Wa.me/${ofcbot}
-*༺═──────────────═༻*
-📧 *Correo:*
-${correo}
-༺═──────────────═༻
-😊 *Asistencia:*
-${asistencia}
-༺═──────────────═༻`
-
-await conn.sendFile(m.chat, imagen1, 'yaemori.jpg', owner, fkontak)
 
 }
-handler.help = ['owner', 'creator']
+
+handler.help = ['creador']
 handler.tags = ['info']
 handler.command = ['creador', 'creator', 'owner', 'propietario', 'dueño']
+
 
 export default handler
 
