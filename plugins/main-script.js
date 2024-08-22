286fb45beed63a8037f76293b402c17bbc44207a
Dev Diego <151096440+Dev-Diego@users.noreply.github.com>
2024-08-21 20:05:24 -0500

    Actualizar main-script.js

--- plugins/main-script.js
+++ plugins/main-script.js
@@ -2,7 +2,7 @@ import moment from 'moment-timezone'
 import fetch from 'node-fetch'
 
 let handler = async (m, { conn, args }) => {
-let res = await fetch('https://api.github.com/repos/OfcDiego/YaemoriBot-MD')
+let res = await fetch('https://api.github.com/repos/Dev-Diego/YaemoriBot-MD')
 let json = await res.json()
 try {
 let txt = `*乂  S C R I P T  -  M A I N  乂*\n\n`
