35e604e6c0b05cee7fe173a87440acaffd40a90b
𝕷𝖔𝖇𝖔 <playperfect344@gmail.com>
2024-08-25 22:52:16 -0300

    Update rg-verificar.js

--- plugins/rg-verificar.js
+++ plugins/rg-verificar.js
@@ -14,7 +14,7 @@ let handler = async function (m, { conn, text, usedPrefix, command }) {
   if (!age) return m.reply('🚩 La edad no puede estar vacía.')
   if (name.length >= 100) return m.reply('🚩 El nombre es demasiado largo.' )
   age = parseInt(age)
-  if (age > 100) return m.reply('👴🏻 Wow el abuelo quiere jugar al bot.')
+  if (age > 999) return m.reply('👴🏻 Wow el abuelo quiere jugar al bot.')
   if (age < 5) return m.reply('🚼  hay un abuelo bebé jsjsj. ')
   user.name = name.trim()
   user.age = age
