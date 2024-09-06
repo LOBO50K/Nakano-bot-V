b2a49f26b00dcddab7894bf205c9361b46071aa4
𝕷𝖔𝖇𝖔 <playperfect344@gmail.com>
2024-09-06 02:13:51 -0300

    Update main-allfake.js

--- plugins/main-allfake.js
+++ plugins/main-allfake.js
@@ -29,7 +29,7 @@ console.log(`Error : ${e}`)
 
 let pp = ''
 let who = m.mentionedJid && m.mentionedJid[0] ? m.mentionedJid[0] : m.fromMe ? conn.user.jid : m.sender
-//let pp = await conn.profilePictureUrl(who, 'image').catch(_ => 'https://telegra.ph/file/327f6ad853cb4f405aa80.jpg')
+//let pp = await conn.profilePictureUrl(who, 'image').catch(_ => 'https://telegra.ph/file/ce156d79ec3842129c60b.jpg')
 
 //creador y otros
 global.creador = 'Wa.me/593405480284'
